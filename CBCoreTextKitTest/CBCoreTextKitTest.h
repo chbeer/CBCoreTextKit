b612c0a55f7e286fe220a357d229c831cfb9db6e
Christian Beer <christian.beer@chbeer.de>
2012-11-16 06:49:30 +0100

    Added empty tests… to be done

---
+++ CBCoreTextKitTest/CBCoreTextKitTest.h
@@ -0,0 +1,13 @@
+//
+//  CBCoreTextKitTest.h
+//  CBCoreTextKitTest
+//
+//  Created by Christian Beer on 03.11.12.
+//  Copyright (c) 2012 Christian Beer. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CBCoreTextKitTest : SenTestCase
+
+@end
---
+++ CBCoreTextKitTest/CBCoreTextKitTest.m
@@ -0,0 +1,30 @@
+//
+//  CBCoreTextKitTest.m
+//  CBCoreTextKitTest
+//
+//  Created by Christian Beer on 03.11.12.
+//  Copyright (c) 2012 Christian Beer. All rights reserved.
+//
+
+#import "CBCoreTextKitTest.h"
+
+#import "NSAttributedString+Markdown.h"
+
+@implementation CBCoreTextKitTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+
+@end
